cf4e434fa8eebee1f283808ac57c75ab0eccce66
Saket Khandelwal <saketdecade17@gmail.com>
2020-04-26 10:05:23 +1000

    Finished Harvester and added DockerFile

--- harvester/crawler.py
+++ harvester/crawler.py
@@ -1,9 +1,10 @@
 from tweepy import Stream, error, Cursor
 from tweepy.streaming import StreamListener
-from utils import lang_list, polygon_list, twitter_setup
+from utils import lang_list, polygon_list, twitter_setup, check_relevance, filter_tweet
 from shapely.geometry import Point, box
 from queue import PriorityQueue
-import json
+import json, time
+import GetOldTweets3 as got
 
 
 class Crawler(StreamListener):
@@ -13,12 +14,42 @@ class Crawler(StreamListener):
         self.api.verify_credentials()
         logger.info("Authentication OK")
         self.twitterStream = None
+
         self.polygon = polygon_list(config['POLYGON'])
         self.languages = lang_list(config['LANG'])
-        self.searchTerms = "covid"
-        self.history = {'tweet': set(), 'user': set()}
+        self.searchTerms = config['SEARCH_TERMS']
+        self.query = self.searchTerms.replace(","," OR ")
+        self.searchTermsList = self.searchTerms.split(',')
         self.searchRadius = config['GEOCODE']
+        self.filterKeys = ["created_at","id","id_str","full_text","coordinates","place","lang"]
+        user = ["id","id_str","created_at","name","screen_name","location","time_zone","statuses_count","followers_count","url"]
+        user_keys = ["user."+k for k in user]
+        self.filterKeys.extend(user_keys)
+
         self.q = PriorityQueue()
+        self.oldTweetSize = 100000
+
+        # Load History of downloaded files
+        temp_1 = set()
+        temp_2 = set()
+        temp_3 = set()
+        try:
+            with open('../../test/data.json','r') as historyF:
+                history = json.load(historyF)
+            for i in history:
+                temp_1.add(int(i['id']))
+                temp_2.add(int(i['user_id']))
+                temp_3.add(int(i['user_id']))
+        except Exception:
+            self.logger.info("File History not found starting empty.")
+
+
+        self.history = {'tweet': temp_1, 'user_q': temp_2, 'user_p': temp_3}
+
+        self.file = open('data.json','a')
+        self.file.write("[")
+        
+        self.twtCount = 0
 
     def check_coordinate(self, coordinates):
         if coordinates:
@@ -34,37 +65,25 @@ class Crawler(StreamListener):
             return False
 
     def add_to_db(self, tweet, db, pipe):
+        self.twtCount += 1
+        
+        # Test writing to file
+        try:
+            self.file.write(json.dumps(tweet))
+            self.file.write(",")
+        except Exception as e:
+            self.logger.exception(e)
+
+
         self.logger.info(
-            f'Pipe: {pipe} | Saving Tweet ID: {tweet["id"]} | Database: {db}')
+            f'Pipe: {pipe} | Saving Tweet ID: {tweet["id"]} | Database: {db} | Tweet Count: {self.twtCount}')
 
     def add_user_to_queue(self, user, flag, pipe):
-        if user['id_str'] not in self.history['user']:
-            self.history['user'].add(user['id_str'])
+        if int(user['id_str']) not in self.history['user_q']:
+            self.history['user_q'].add(int(user['id_str']))
             self.q.put((flag, user['id_str']))
             self.logger.info(
                 f"Pipe: {pipe} | Added User ID: {user['id_str']} to Queue")
-
-    def tweet_processor(self, tweet, flag, pipe):
-        self.logger.debug(
-            f"Pipe: {pipe} | Processing Tweet ID: {tweet['id']} | User ID: {tweet['user']['id']}")
-
-        if tweet['id'] not in self.history['tweet']:
-            self.history['tweet'].add(tweet['id'])
-
-        # TO-DO add COUCHDB functions here.......
-            if self.check_coordinate(tweet['coordinates']):
-                # TO-DO save to COUCH DB
-                self.add_to_db(tweet, "BLAH", pipe)
-                self.add_user_to_queue(tweet['user'], flag, pipe)
-                return True
-
-            else:
-                return False
-
-        else:
-            self.logger.info(
-                f"Pipe: {pipe} | Skipping Tweet ID: {tweet['id']} as already processed.")
-            return False
 
     def on_connect(self):
         """
@@ -78,14 +97,22 @@ class Crawler(StreamListener):
         """
         if status.retweeted:
             return
+        
+        tweet = status._json
+
+        if hasattr(status, "extended_tweet"):
+            tweet['full_text'] = status.extended_tweet['full_text']
+        else:
+            tweet['full_text'] = status.text
 
         try:
-            if self.tweet_processor(status._json, 0, "Stream"):
-                self.logger.info(
-                    f"Pipe: Stream | Added Tweet ID: {status.id} from User ID: {status.author.id}")
+            if self.tweet_processor(tweet, 0, "Stream"):
+                self.logger.debug(
+                        f"Pipe: Stream | Added Tweet ID: {status.id} by User ID: {status.author.id}"
+                    )
         except Exception:
             self.logger.info(
-                f"Pipe: Stream | Error processing tweet... Skipping....")
+                f"Pipe: Stream | Error processing tweet ID: {status.id}... Skipping....")
 
     def on_error(self, status_code):
         """
@@ -109,83 +136,156 @@ class Crawler(StreamListener):
             self.twitterStream.disconnect()
             self.twitterStream = None
 
+    def tweet_processor(self, tweet, flag, pipe):
+        self.logger.debug(
+            f"Pipe: {pipe} | Processing Tweet ID: {tweet['id']} | User ID: {tweet['user']['id']}")
+
+        tweet = filter_tweet(tweet, self.filterKeys)
+
+        if tweet['id'] not in self.history['tweet']:
+            self.history['tweet'].add(tweet['id'])
+
+        # TO-DO add COUCHDB functions here.......
+            if self.check_coordinate(tweet['coordinates']) and check_relevance(tweet['full_text'],self.searchTermsList):
+                # TO-DO save to COUCH DB
+                self.add_to_db(tweet, "BLAH", pipe)
+                self.add_user_to_queue(tweet['user'], flag, pipe)
+                return True
+            else:
+                return False
+
+        else:
+            self.logger.debug(
+                f"Pipe: {pipe} | Skipping Tweet ID: {tweet['id']} as already processed.")
+            return False
+
     def download_stream(self):
         self.logger.info(
             "Pipe: Stream | Initializing Twitter Streaming pipeline......")
         self.twitterStream = Stream(self.api.auth, self, tweet_mode='extended')
         self.twitterStream.filter(locations=self.polygon,
             is_async=True,
-            languages=self.languages,
-            track = [self.searchTerms])
+            languages=self.languages)
 
     def download_search(self):
-        self.logger.info(
-            "Pipe: Search | Initializing Twitter Search pipeline......")
-        query = self.searchTerms
+        query = self.query
         geocode = self.searchRadius
+        maxTweets = 1000000
+        twtPerQuery = 100
+        current = self.twtCount
+
+        self.logger.info(
+            f"Pipe: Search | Initializing Twitter Search pipeline for {maxTweets} tweets......")
 
         for tweet in Cursor(
                 self.api.search,
                 q=query,
-                count=100,
+                count=twtPerQuery,
                 geocode=geocode,
                 tweet_mode='extended',
+                lang = 'en',
                 exclude_retweets=True,
                 exclude_replies=True).items():
 
             try:
                 if self.tweet_processor(tweet._json, 1, "Search"):
-                    self.logger.info(
-                        f"Pipe: Search | Added Tweet ID: {tweet.id} from User ID: {tweet.author.id}")
+                    self.logger.debug(
+                        f"Pipe: Search | Added Tweet ID: {tweet.id} by User ID: {tweet.author.id}"
+                    )
             except Exception:
                 self.logger.info(
-                    f"Pipe: Search | Error processing tweet... Skipping....")
+                    f"Pipe: Search | Error processing tweet ID: {tweet.id} ... Skipping....")
 
+        count = self.twtCount - current
         self.logger.info(
-            "Pipe: Search | Closing Twitter Search pipeline......")
+            f"Pipe: Search | Closing Twitter Search pipeline with {count} valid tweets......")
 
     def download_user(self):
         self.logger.info(
             "Pipe: User   | Initializing Twitter User Timeline pipeline......")
         while True:
-            user_id = self.q.get(block=True, timeout=None)[1]
+            user_id = self.q.get(block=True,timeout=None)[1]
             user_id = int(user_id)
             relevant = False
             item_count = 0
 
-            if user_id not in self.history['user']:
-                self.history['user'].add(int(user_id))
+            if user_id not in self.history['user_p']:
+                self.history['user_p'].add(user_id)
 
                 for tweet in Cursor(self.api.user_timeline,
                     user_id=user_id,
                     count=200,
                     tweet_mode='extended',
+                    lang = 'en',
                     exclude_retweets=True,
                     exclude_replies=True).items():
                     item_count += 1
                     try:
                         if self.tweet_processor(tweet._json, 1, "User"):
+                            self.logger.debug(
+                                f"Pipe: User | Added Tweet ID: {tweet.id} by User ID: {tweet.author.id}"
+                            )
                             relevant = True
-                            self.logger.info(
-                                f"Pipe: User   | Added Tweet ID: {tweet.id} from User ID: {tweet.author.id}")
                     except Exception:
                         self.logger.info(
-                            f"Pipe: User   | Error processing tweet... Skipping....")
+                            f"Pipe: User   | Error processing tweet ID: {tweet.id} ... Skipping....")
 
-                    if item_count == 300 and not relevant:
-                        self.logger.info(f"Pipe: User | User ID: {user_id} No relevant tweets found")
+                    if item_count == 500 and not relevant:
+                        self.logger.info(f"Pipe: User | User ID: {user_id} No relevant tweets found.")
                         break
+
+                if relevant:
+                    for follower in Cursor(self.api.followers,user_id=user_id).items(300):
+                        self.add_user_to_queue(follower._json,2,"User")
+
+            else:
+                self.logger.debug(
+                    f"Pipe: User   | Already Processed User ID: {user_id} ... Skipping....")
+
+    def download_old_tweets(self):
+        self.logger.info(
+            "Pipe: Old | Initializing Twitter Old Tweets pipeline......")
+        query = self.searchTermsList
+
+        for term in query:
+            self.logger.info(
+            f"Pipe: Old | Searching term: {term}.....")
+            try:
+                tweetCriteria = got.manager.TweetCriteria().setQuerySearch(term)\
+                                            .setSince("2019-05-01")\
+                                            .setUntil("2020-04-25")\
+                                            .setNear('Melbourne, Australia')\
+                                            .setWithin('50km')\
+                                            .setMaxTweets(self.oldTweetSize)
+                tweets = got.manager.TweetManager.getTweets(tweetCriteria)
+
+                for tweet in tweets:
+                    self.add_user_to_queue({'id_str':tweet.author_id},1,"Old")
+            except SystemExit:
+                print('Pipe: Old | Reached limit exiting...')
+                break
+            
 
 
     def start_pipeline(self):
         try:
-            self.download_stream()
-            self.download_search()
-            self.download_user()
-        except error.TweepError as e:
-            self.logger.exception(e)
+            while True:
+                try:
+                    self.download_stream()
+                    self.download_search()
+                    self.download_old_tweets()
+                    self.download_user()
+                except error.TweepError as e:
+                    self.logger.exception(e)
+                    self.disconnect()
+                    time.sleep(50)
+                except Exception as e:
+                    self.logger.exception(e)
+                    self.disconnect()
+                    time.sleep(50)
+                    break
         except KeyboardInterrupt:
+            self.file.write("]")
+            self.file.close()
             self.disconnect()
             self.logger.info('Stopping Crawler......')
-        except Exception as e:
-            self.logger.exception(e)
--- harvester/utils.py
+++ harvester/utils.py
@@ -1,8 +1,9 @@
-from tweepy import  OAuthHandler, API
+from tweepy import OAuthHandler, API
 import os
 import logging.config
 import yaml
-
+import re
+from benedict import benedict
 
 
 # Code adapted from https://fangpenlin.com/posts/2012/08/26/good-logging-practice-in-python/
@@ -25,21 +26,33 @@ def setup_logging(
     else:
         logging.basicConfig(level=default_level)
 
+
 def lang_list(languages):
     languages = languages.split(',')
     return languages
+
 
 def polygon_list(polygon):
     polygon = [float(i) for i in polygon.split(',')]
     return polygon
 
+
 def twitter_setup(config):
     auth = OAuthHandler(config['C_KEY'], config['C_SECRET'])
     auth.set_access_token(config['A_TOKEN'], config['A_SECRET'])
-    api = API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify= True)
+    api = API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
     return api
 
-def filter(tweet):
-    filtered = {
 
-    }
+def filter_tweet(tweet, keys):
+    filtered = benedict(tweet).subset(keys=keys)
+    return filtered
+
+def check_relevance(tweet,words):
+    tweet = tweet.lower()
+    words = [word.lower() for word in words]
+    words_re = re.compile("|".join(words))
+    flag = False
+    if words_re.search(tweet):
+        flag = True
+    return flag
